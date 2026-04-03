91bebf25e66075988c1cfcf94bbbf854d2c26cb7
xumi1993 <gomijianxu@gmail.com>
2026-04-03 15:13:41 -0400

    Add run_this_test.sh script for SURFATT_tomo with checkerboard input parameters

---
+++ testcase04/run_this_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
+
+cp ../examples/00_checkerboard_iso/input_params.yml ./
+cp ../examples/00_checkerboard_iso/src_rec_file_*.csv ./
+
+# upadate parameters with PyTomoATT
+pta setpar input_params.yml data.vel_type "[True, True]"
+pta setpar input_params.yml inversion.init_model_type 1
+
+mpirun -np 8 $SRUFATT_HOME/SURFATT_tomo -i input_params.yml -f
