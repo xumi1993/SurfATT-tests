3b40a0aab6965c090c4e15250ad0f80b648c8a0c
xumi1993 <gomijianxu@gmail.com>
2026-04-05 15:50:14 -0400

    Refactor code structure for improved readability and maintainability

---
+++ testcase12/run_this_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
+
+NPROC=8
+
+input_params=input_params.yml
+cp ../examples/00_checkerboard_iso/$input_params ./
+
+# upadate parameters with PyTomoATT
+pta setpar $input_params data.src_rec_file_ph src_rec_test_data_PH.csv
+pta setpar $input_params data.vel_type "[True, False]"
+pta setpar $input_params postproc.smooth_method 0
+pta setpar $input_params inversion.is_anisotropy False
+pta setpar $input_params inversion.optim_method 1
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params
