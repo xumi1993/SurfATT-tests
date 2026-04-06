01b085363008fc7de66b7b4b12283ce849b9e350
xumi1993 <gomijianxu@gmail.com>
2026-04-06 15:55:10 -0400

    Add run_this_test.sh script for SurfATTPP test case 13
    - Create a new shell script to automate the execution of SurfATTPP for test case 13.
    - Set parameters for the forward modeling and inversion processes using PyTomoATT.
    - Include necessary file copying and parameter setting for the test.
    - Utilize MPI for parallel processing with specified number of processors.

---
+++ testcase13/run_this_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
+
+NPROC=8
+MAXIT=40
+
+input_params=input_params.yml
+cp ../examples/00_checkerboard_iso/$input_params ./
+cp ../examples/00_checkerboard_iso/src_rec_file_ph.csv ./
+
+# upadate parameters with PyTomoATT
+# pta setpar $input_params data.src_rec_file_ph src_rec_test_data_PH.csv
+pta setpar $input_params data.src_rec_file_ph src_rec_file_ph.csv
+pta setpar $input_params data.vel_type "[True, False]"
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_cb_fwd -i $input_params -n 2/3/2 -m 0.2 -p 0.08
+
+pta setpar $input_params data.src_rec_file_ph OUTPUT_FILES/src_rec_file_forward_PH.csv
+pta setpar $input_params postproc.smooth_method 0
+pta setpar $input_params inversion.is_anisotropy False
+pta setpar $input_params inversion.init_model_type 1
+pta setpar $input_params inversion.optim_method 1
+pta setpar $input_params inversion.niter $MAXIT
+
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params
