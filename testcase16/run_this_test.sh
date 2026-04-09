cc6ee1c10494c2db67c2d76de1526fc25f655a54
xumi1993 <gomijianxu@gmail.com>
2026-04-09 17:15:19 -0400

    Add missing line to execute build_h5_model.py in run_this_test.sh

--- testcase16/run_this_test.sh
+++ testcase16/run_this_test.sh
@@ -6,5 +6,7 @@ MAXIT=40
 SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
 input_params=input_params.yaml
 
+python build_h5_model.py
+
 # inversion 
 mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params -f
