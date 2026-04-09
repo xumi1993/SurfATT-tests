4561cf58b3f6b99f6cde18610e4b4923aed560e7
xumi1993 <gomijianxu@gmail.com>
2026-04-09 19:35:02 -0400

    Add parameter setting for src_rec_file_ph in run_this_test.sh

--- testcase16/run_this_test.sh
+++ testcase16/run_this_test.sh
@@ -8,5 +8,8 @@ input_params=input_params.yaml
 
 python build_h5_model.py
 
+pta setpar $input_params data.src_rec_file_ph src_rec_file_ph.csv
+head -n 1000 src_rec_data_wus.csv > src_rec_file_ph.csv
+
 # inversion 
 mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params -f
