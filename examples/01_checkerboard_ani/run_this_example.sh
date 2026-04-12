bd7fef33bdf8624d570c3d33f1f5d8248c261253
xumi1993 <gomijianxu@gmail.com>
2026-04-11 20:54:02 -0400

    Update run script to use SRUFATT_HOME variable and correct file paths

--- examples/01_checkerboard_ani/run_this_example.sh
+++ examples/01_checkerboard_ani/run_this_example.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 NPROC=8
+SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
 
 mkdir -p OUTPUT_FILES
-cp src_rec_file_100.csv OUTPUT_FILES/src_rec_file_forward_ph.csv
+cp src_rec_file_100.csv src_rec_test_data_PH.csv
 
 # create 2x3x2 checkers and forward simulate surface traveltimes
-mpirun -np $NPROC ../../bin/surfatt_cb_fwd -i input_params.yml -n 3/3/2 -A 2/2/2 -m 0.2 -p 0.08 -a 0.08/135 -s 5
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_cb_fwd -i input_params_fwd.yml -n 3/3/2 -a 2/2/2/135 -m 0.2 -p 0.08/0.004 -s 5
 
 # inversion 
-mpirun -np $NPROC ../../bin/surfatt_tomo -i input_params.yml
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i input_params.yml
