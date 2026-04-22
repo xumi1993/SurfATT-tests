e6a1fe8c7f7a466018d8df79f228104d5799df82
xumi1993 <gomijianxu@gmail.com>
2026-04-22 15:28:00 -0400

    Add example files for CSEM inversion setup
    - Created new CSEM input parameter file `input_params.yml` with detailed configuration options for data, output, domain, model, topography, post-processing, and inversion parameters.
    - Added source receiver data file `src_rec_data_wus.csv` for wave propagation simulations.
    - Introduced new example file `csem.nc` for CSEM inversion, though the file is too large to display differences.

--- testcase17/run_this_test.sh
+++ testcase17/run_this_test.sh
@@ -5,11 +5,16 @@ MAXIT=40
 
 SRUFATT_HOME=/Users/xumijian/Codes/SurfATTPP/bin
 input_params=input_params.yaml
+base_dir=../examples/05_westUS_ani
+
+cp $base_dir/$input_params ./
+head -n 1000 $base_dir/src_rec_data_wus.csv > src_rec_file_ph.csv
+cp $base_dir/csem.nc ./
 
 python build_h5_model.py
 
 pta setpar $input_params data.src_rec_file_ph src_rec_file_ph.csv
-head -n 1000 src_rec_data_wus.csv > src_rec_file_ph.csv
+pta setpar $input_params domain.grid_method 1
+pta setpar $input_params inversion.niter 3
 
-# inversion 
-mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params -f
+mpirun -np $NPROC $SRUFATT_HOME/SURFATT_tomo -i $input_params
